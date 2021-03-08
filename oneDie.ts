702654b8625c8be3e51cb895af480d3224f9c265
Matt Deeds <mdeeds@gmail.com>
2021-03-07 20:59:14 -0800

    Allow for multiple players in the game.

--- oneDie.ts
+++ oneDie.ts
@@ -7,21 +7,25 @@ export class OneDie implements Game {
   static kScoreIndex = 0;
   static kRoundIndex = 1;
   static kTotalScore = 2;
+  static kStatePerPlayer = 3;
 
   private roundCount: number;
   private winningScore: number;
-  constructor(roundCount = 5, winningScore = 25) {
+  private numberOfPlayers: number;
+
+  constructor(roundCount = 5, winningScore = 25, numberOfPlayers = 1) {
     this.roundCount = roundCount;
     this.winningScore = winningScore;
+    this.numberOfPlayers = numberOfPlayers;
   }
 
   getInitialState() {
-    return new Float32Array(3);
+    return new Float32Array(OneDie.kStatePerPlayer * this.numberOfPlayers);
   }
 
   // [ current score, current round, totalScore]
   getStateSize() {
-    return 3;
+    return OneDie.kStatePerPlayer * this.numberOfPlayers;
   }
 
   // There are two possible moves: go or end round.
@@ -34,7 +38,7 @@ export class OneDie implements Game {
   }
 
   applyMove(state: Float32Array, move: Float32Array): Float32Array {
-    const newState = new Float32Array(state);
+    const newState: Float32Array = state.slice(0, OneDie.kStatePerPlayer);
     if (move[OneDie.kGoIndex] > move[OneDie.kEndIndex]) {
       // "GO" move
       const newRoll = this.getDieRoll();
@@ -50,15 +54,42 @@ export class OneDie implements Game {
       newState[OneDie.kRoundIndex] += 1;
       newState[OneDie.kScoreIndex] = 0;
     }
-    return newState;
+
+    // Result state is shifted by one player to the left.  The next active
+    // player is placed at the front, and the current player is placed at the
+    // end.
+    const resultState = new Float32Array(this.getStateSize());
+    const otherPlayerStateSize =
+      (this.numberOfPlayers - 1) * OneDie.kStatePerPlayer;
+    for (let i = 0; i < otherPlayerStateSize; ++i) {
+      resultState[i] = state[i + OneDie.kStatePerPlayer];
+    }
+    for (let i = 0; i < OneDie.kStatePerPlayer; ++i) {
+      resultState[i + otherPlayerStateSize] = newState[i];
+    }
+    return resultState;
+  }
+
+  private isWinningAtOffset(state: Float32Array, offset: number) {
+    return (state[OneDie.kRoundIndex + offset] == this.roundCount &&
+      state[OneDie.kTotalScore + offset] >= this.winningScore);
   }
 
   isWinning(state: Float32Array) {
-    return (state[OneDie.kRoundIndex] == this.roundCount &&
-      state[OneDie.kTotalScore] >= this.winningScore);
+    return this.isWinningAtOffset(state, 0);
   }
   isLosing(state: Float32Array) {
-    return (state[OneDie.kRoundIndex] == this.roundCount &&
-      state[OneDie.kTotalScore] < this.winningScore);
+    // If we are winning, we are not losing.
+    if (this.isWinningAtOffset(state, 0)) {
+      return false;
+    }
+    // If someone else has won, we have lost.
+    for (let i = 1; i < this.numberOfPlayers; ++i) {
+      if (this.isWinningAtOffset(state, i * OneDie.kStatePerPlayer)) {
+        return true;
+      }
+    }
+    // Otherwise, we can lose if all rounds are used.
+    return (state[OneDie.kRoundIndex] == this.roundCount);
   }
 }
