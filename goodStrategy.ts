c6c1a0a21761a2430659cef4cdb7b6debc693a28
Matt Deeds <mdeeds@gmail.com>
2021-03-12 21:50:21 -0800

    Small fix to "good" strategy

--- goodStrategy.ts
+++ goodStrategy.ts
@@ -12,7 +12,7 @@ export class GoodStrategy implements Strategy {
   getMove(state: State): Move {
     const move = new Move(this.game.getMoveSize());
     if (state.data[OneDie.kScoreIndex] >= 12 ||
-      state.data[OneDie.kTotalScore] > 25) {
+      state.data[OneDie.kTotalScore] + state.data[OneDie.kScoreIndex] > 25) {
       move.data[OneDie.kEndIndex] = 1.0;
     } else {
       move.data[OneDie.kGoIndex] = 1.0;
